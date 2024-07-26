2a91fd4a0d00c4e6cbdf0c5ccb77e75f1497dc92
darkhorsedev012 <darkhorsedev012@outlook.com>
2024-07-26 17:38:19 -0300

    update

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,55 +1,61 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 const pointSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        enum: ['Point']
-    },
-    coordinates: {
-        type: [Number]
-    }
+  type: {
+    type: String,
+    enum: ["Point"],
+  },
+  coordinates: {
+    type: [Number],
+  },
 });
 
-const addressSchema = new mongoose.Schema({
+const addressSchema = new mongoose.Schema(
+  {
     user: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
     label: {
-        type: String,
-        trim: true,
-        enum:['home','office','other']
+      type: String,
+      trim: true,
+      enum: ["home", "office", "other"],
     },
-    region: {//pradesh
-        type: String,
-        trim: true,
+    region: {
+      //pradesh
+      type: String,
+      trim: true,
     },
     city: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
-    area: {//tole,area name
-        type: String,
-        trim: true,
+    area: {
+      //tole,area name
+      type: String,
+      trim: true,
     },
-    address: {//street level address
-        type: String,
-        trim: true,
+    address: {
+      //street level address
+      type: String,
+      trim: true,
     },
     geolocation: {
-        type: pointSchema,
+      type: pointSchema,
     },
     phoneno: {
-        type: String,
-        trim: true,
-        max: 9999999999,
+      type: String,
+      trim: true,
+      max: 9999999999,
     },
-    isActive:{
-        type: Date,
-        default: null
-    }
-}, { timestamps: true });
+    isActive: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 addressSchema.index({ geolocation: "2dsphere" });
 
 module.exports = mongoose.model("address", addressSchema);
